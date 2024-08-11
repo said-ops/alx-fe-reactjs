b9f0bb611147f60419fda8cb685792a9dc10bd07
said-ops <idrissisaid29@gmail.com>
2024-08-11 20:18:14 +0100

    Create Specific Components in a React Application

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+    return (
+        <footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    )
+}
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+    return (
+        <header>
+            <h1>My Favourite Cities</h1>
+        </header>
+    );
+}
