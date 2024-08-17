eae1a984b7db21f92e23f194454830dadad6d5ca
said-ops <idrissisaid29@gmail.com>
2024-08-17 23:37:41 +0100

    context api e

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react';
 import UserInfo from './UserInfo';
+import UserContext from './userContext';
 
 function ProfilePage({ userData }) {
   return <UserInfo  />;
