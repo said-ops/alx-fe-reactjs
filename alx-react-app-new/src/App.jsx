0e0a32ed76faa0ab0aad041a78f3725ea28e8285
said-ops <idrissisaid29@gmail.com>
2024-08-17 22:40:59 +0100

    Counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +22,7 @@ function App() {
       age="25"
       bio="Loves hiking and photography"
       />
+      <Counter />
     </>
   )
 }
--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <header  style = {{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-            <h1>My Favorite Cities</h1>
+            <h1 >My Favorite Cities</h1>
         </header>
     );
 }
