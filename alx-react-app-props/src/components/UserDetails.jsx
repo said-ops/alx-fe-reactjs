06666a0e95701acfcfb9e4a545124af81d256853
said-ops <idrissisaid29@gmail.com>
2024-08-17 23:30:48 +0100

    context api

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React,{useContext} from "react";
-import  UserContext  from "./userContext";
+import  UserContext  from "./UserContext";
 function UserDetails({ userData }) {
     const {name,email} = useContext(UserContext)
     return (
