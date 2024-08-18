cd8522982c25fe7966600e66d9c3a582e35b6ab3
said-ops <idrissisaid29@gmail.com>
2024-08-18 15:34:29 +0100

    adding contact.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 
