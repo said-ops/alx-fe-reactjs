df47a72d1cd9f68739a193e72d0f953733f4c953
said-ops <idrissisaid29@gmail.com>
2024-08-24 16:26:06 +0100

    repo init

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
