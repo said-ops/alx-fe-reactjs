233d6dde3d06bf7d3799f3f801ee8f0e22cd53ff
said-ops <idrissisaid29@gmail.com>
2024-08-30 19:21:25 +0100

    form formik 1

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import RegistrationForm from './components/RegistrationForm'
-import FormikForm from './components/formikForm.js'
+import FormikForm from './components/formikForm'
 
 function App() {
   return (
