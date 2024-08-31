8112af6dc49200454d336cbaac4d3cbf6ad52961
said-ops <idrissisaid29@gmail.com>
2024-08-31 22:53:16 +0100

    router setup

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter as Router ,Routes,Route,Link} from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="">Home</Link></li>
+        </ul>
+      </nav>
+      
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
