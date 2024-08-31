304edd02739a5a7fe13d60524bedf41aaeb75ef5
said-ops <idrissisaid29@gmail.com>
2024-08-31 22:56:13 +0100

    setup router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router ,Routes,Route,Link} from 'react-router-dom'
+import {BrowserRouter as Router,Route ,Routes,Link} from 'react-router-dom'
 import Home from './components/Home'
 
 function App() {
