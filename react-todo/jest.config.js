9922b7a71b5b6e143d195ab6c961145e4db6cb7c
said-ops <idrissisaid29@gmail.com>
2024-09-01 03:19:40 +0100

    repo init

---
+++ react-todo/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+      '^.+\\.[jt]sx?$': 'babel-jest',
+    },
+  };
+  
