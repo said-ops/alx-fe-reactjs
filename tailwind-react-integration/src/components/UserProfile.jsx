dc091c451e69d6176c486196ec6bdeb26534ca57
said-ops <idrissisaid29@gmail.com>
2024-09-04 18:47:16 +0100

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
     return (
       <div className="user-profile bg-gray-100
                 max-w-sm p-8 mx-auto my-20 rounded-lg
-                shadow-lg sm:p-4 md:p-8 sm:max-w-sm md:max-w-sm
+                shadow-lg sm:p-4 md:p-8 sm:max-w-xm md:max-w-sm
       ">
         <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto
         sm:w-24 sm:h-24 md:w-36 md:h-36" />
