ef4b043188bee9109c51b56a8677533703deb920
said-ops <idrissisaid29@gmail.com>
2024-09-10 15:56:46 +0100

    repo init

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-9xl  bg-gradient-to-r from-green-950 to-green-400 bg-clip-text text-transparent border-t-2'>Tailwind Project</h1>
+      <p className='text-blue-500 border-t border-red-300'>This is a blue paragraph</p>
+    </>
+  )
+} 
+
+export default App
