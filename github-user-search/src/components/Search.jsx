24c5cd2383765f91415f270c0b6120c90573c97b
said-ops <idrissisaid29@gmail.com>
2024-09-19 18:22:29 +0100

    fetch data

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,19 +1,73 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import fetchUserData from '../services/githubService'
 
 function Search() {
 
-  useEffect( ()=>{
-    // console.log('GitHub Token:', import.meta.env.VITE_GITHUB_API_KEY);
+  const [username,setUser]=useState('')
+  const [data,setData]=useState([])
 
-   const fetchData = async ()=>{
-    const data= await fetchUserData('said-ops')
-   console.log(data)
+  // useEffect( ()=>{
+  //   // console.log('GitHub Token:', import.meta.env.VITE_GITHUB_API_KEY);
+  //  const fetchData = async ()=>{
+  //   const data= await fetchUserData('said-ops')
+  //     console.log(data)
+  //  }
+  //  fetchData();
+  // },[])
+
+  //fetch data based on user input
+  const fetchData = async (searchTerm)=>{
+    const data= await fetchUserData(searchTerm)
+      setData(data)
    }
-   fetchData();
-  },[])
+
+  //handle submit and change
+  const handlechange = (e)=>{
+    setUser(e.target.value)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    if(username){
+      fetchData(username)
+      console.log(data)
+    }
+  }
+
   return (
-    <h2>this is search component</h2>
+    <>
+      <div className="container">
+
+        <div>
+          <h2>DevFinder</h2>
+          <div>
+            <span>Light</span>
+            <img src="./assets/icon-sun.svg" alt="light mode" />
+          </div>
+        </div>
+        {/* form goes here */}
+        <form 
+          action="#"
+          onSubmit={(e)=>handleSubmit(e)}
+          >
+           <div>
+            <label htmlFor="username">
+              <img src="./assets/search-icon.svg" alt="search" />
+                <input 
+                type="text"
+                name='username'
+                id='username'
+                value={username}
+                onChange={e=>handlechange(e)} 
+                />
+            </label>
+            <button type='submit'>Search</button>
+           </div>
+
+        </form>
+      </div>
+
+      
+    </>
   )
 }
 
